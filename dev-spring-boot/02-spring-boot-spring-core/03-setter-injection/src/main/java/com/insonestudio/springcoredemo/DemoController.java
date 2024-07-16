504307122944ffd4a032e9f93b4207f5c83d20ff
Samira Marcela Roman <samira1980dev@gmail.com>
2024-07-16 18:06:19 -0300

    Setter injection

--- dev-spring-boot/02-spring-boot-spring-core/03-setter-injection/src/main/java/com/insonestudio/springcoredemo/DemoController.java
+++ dev-spring-boot/02-spring-boot-spring-core/03-setter-injection/src/main/java/com/insonestudio/springcoredemo/DemoController.java
@@ -1,22 +1,19 @@
-package com.insonestudio.springcoredemo.rest;
+package com.insonestudio.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.insonestudio.springcoredemo.common.Coach;
 
 @RestController
 public class DemoController {
 
 	private Coach myCoach;
 
-
 	@Autowired
-	public void setCoach(Coach theCoach) {
+	public DemoController(Coach theCoach) {
 		myCoach = theCoach;
 	}
-	
+
 	@GetMapping("/dailyworkout")
 	public String getDailyWorkout() {
 		return myCoach.getDailyWorkout();
