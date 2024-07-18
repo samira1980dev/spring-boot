3b93545c590f402ae66f6b62ded8f63f7ee91b54
Samira Marcela Roman <samira1980dev@gmail.com>
2024-07-18 16:24:30 -0300

    Qualifiers

--- dev-spring-boot/02-spring-boot-spring-core/04-Qualifiers/src/main/java/com/insonestudio/springcoredemo/DemoController.java
+++ dev-spring-boot/02-spring-boot-spring-core/04-Qualifiers/src/main/java/com/insonestudio/springcoredemo/DemoController.java
@@ -1,23 +1,19 @@
-package com.insonestudio.springcoredemo.rest;
+package com.insonestudio.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.insonestudio.springcoredemo.common.Coach;
 
 @RestController
 public class DemoController {
 
 	private Coach myCoach;
 
-
 	@Autowired
-	public void DemoController (@Qualifier("baseballCoach") Coach theCoach) {
+	public DemoController(Coach theCoach) {
 		myCoach = theCoach;
 	}
-	
+
 	@GetMapping("/dailyworkout")
 	public String getDailyWorkout() {
 		return myCoach.getDailyWorkout();
