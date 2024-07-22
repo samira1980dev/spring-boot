b5e6b83f457ab586456d67f422d7d5045e4e4dea
Samira Marcela Roman <samira1980dev@gmail.com>
2024-07-22 09:35:54 -0300

    Bean lifecycle methods

--- dev-spring-boot/02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/insonestudio/springcoredemo/common/CricketCoach.java
+++ dev-spring-boot/02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/insonestudio/springcoredemo/common/CricketCoach.java
@@ -1,15 +1,28 @@
 package com.insonestudio.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
 	public CricketCoach() {
 		System.out.println("In constructor: " + getClass().getSimpleName());
+	}
+
+	// define our init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+	}
+
+	// define our destroy method
+
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
 	}
 
 	@Override
--- dev-spring-boot/02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/insonestudio/springcoredemo/DemoController.java
+++ dev-spring-boot/02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/insonestudio/springcoredemo/DemoController.java
@@ -1,11 +1,8 @@
-package com.insonestudio.springcoredemo.rest;
+package com.insonestudio.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.insonestudio.springcoredemo.common.Coach;
 
 @RestController
 public class DemoController {
@@ -13,8 +10,7 @@ public class DemoController {
 	private Coach myCoach;
 
 	@Autowired
-	public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
-		System.out.println("In constructor: " + getClass().getSimpleName());
+	public DemoController(Coach theCoach) {
 		myCoach = theCoach;
 	}
 
